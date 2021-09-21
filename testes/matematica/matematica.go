568b185e8199a87926427b96cd1d5a535abf2e79
Felipe Lima <felipe.lima@agilize.com.br>
2021-09-21 16:10:51 -0300

    Add Test module

---
+++ testes/matematica/matematica.go
@@ -0,0 +1,23 @@
+package matematica
+
+import (
+	"fmt"
+	"strconv"
+)
+
+// Media é responsavel por calcular o que você ja sabe... :D
+func Media(numeros ...float64) float64 {
+	total := 0.0
+	for _, num := range numeros {
+		total += num
+	}
+
+	media := total / float64(len(numeros))
+	mediaArredondada, _ := strconv.ParseFloat(fmt.Sprintf("%.2f", media), 64)
+	return mediaArredondada
+
+}
+
+// func main() {
+
+// }
